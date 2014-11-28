71033748d2f2f689494c9be783e42a3aeb96b0a6
root <root@salt.hdtr.com>
2014-11-28 18:19:25 +0800

    update

--- python/hillstone.pexpect.py
+++ python/hillstone.pexpect.py
@@ -4,23 +4,37 @@ import pexpect, sys, os, re, datetime
 
 timestamp = "\n######" + datetime.datetime.now().strftime("%Y-%m-%d-%H:%M") + "######\n"
 
-def run_cmd(host, user, password, cmd):
+host = 'internet-fw01'
+user = 'hillstone'
+passwd = 'Tianren@FW1689.7'
+cmd = 'show dnat'
+#cmd = 'show admin host'
+ssh = pexpect.spawn('ssh %s@%s' % (user, host)) 
+#ssh.logfile=sys.stdout
 
-  ssh = pexpect.spawn('ssh %s@%s' % (user, host)) 
-  #ssh.logfile=sys.stdout
-  ssh.expect('.*d:.*')
-  ssh.sendline(password)
-  ssh.expect('.*#.*')
-  ssh.sendline('terminal length 0')
-  ssh.expect('.*# ')
-  ssh.sendline(cmd)
-  ssh.expect('.*# ')
-  dnatrule_list = ssh.after.splitlines()
-  ssh.sendline('\r')
-  ssh.expect('.*# ')
-  ssh.sendline('exit')
-  ssh.close
-  return dnatrule_list
+ssh.expect('.*d:.*')
+ssh.sendline(passwd)
 
+ssh.expect('.*#.*')
+ssh.sendline('terminal length 0')
 
-rules = run_cmd('192.168.9.6', 'pexpect', 'P@$w0rD01!', 'show config | inc dnat')
+ssh.expect('.*# ')
+ssh.sendline(cmd)
+
+ssh.expect('.*# ')
+output = ssh.after.splitlines()
+
+ssh.sendline('\r')
+ssh.expect('.*# ')
+ssh.sendline('exit')
+ssh.close
+
+    
+def show_cmd(output):
+    for line in output[4:-2]:
+      line = line.strip(' ')
+      line = re.sub('\s+', ' ', line)
+      line = re.sub('=+', '', line)
+      print line
+
+show_cmd(output)
